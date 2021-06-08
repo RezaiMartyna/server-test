f8ca0a5d4f9f7a3ec9c565f9401787224124ef56
RezaiMartyna <73740511+RezaiMartyna@users.noreply.github.com>
2021-06-08 12:57:44 +0200

    heroku

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./db');
+const path = require('path');
+
 
 const app = express();
-const { v4: uuidv4 } = require('uuid');
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
+
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,11 +18,15 @@ app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 })
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
